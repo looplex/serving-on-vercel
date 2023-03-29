abfcb84fddf2f510b6106799122d47d07306576d
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2023-03-28 21:41:44 -0300

    fix: client-side rendering should useState and useEffect

--- pages/csr/[slug].js
+++ pages/csr/[slug].js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 import Clock from '../../components/Clock.js'
 
 function CsrPage (props) {
   const router = useRouter()
   // NOTE: on CSR, data fetching happens here, using libraries like SWR or state management libraries actions.
-  const dttm = new Date()
+  const [dttm, setDttm] = useState(null)
+  useEffect(() => {
+    setDttm(new Date(Date.now()))
+  }, [])
   return (
     <main>
       <Head>
@@ -14,7 +18,7 @@ function CsrPage (props) {
       </Head>
       <h1>CSR Page</h1>
       <p>
-        Hi! I am a <code>Client Side Rendered</code> Page born in <mark><time dateTime={dttm.toISOString()}>{dttm.toLocaleString()}</time></mark>, but it&apos;s <mark><Clock /></mark> now.<br />
+        Hi! I am a <code>Client Side Rendered</code> Page born in <mark><time dateTime={dttm?.toISOString()}>{dttm?.toLocaleString()}</time></mark>, but it&apos;s <mark><Clock /></mark> now.<br />
         and my slug is: <code>{router.query.slug}</code>. You can change the <code>any-slug</code> fragment in the address bar to see my content changing. Also, don&apos;t forget to inspect the initial source code.
       </p>
     </main>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 
+import { useState, useEffect } from 'react'
+
 import Clock from '../components/Clock.js'
 
 function IndexPage (props) {
-  const dttm = new Date(Date.now())
+  const [dttm, setDttm] = useState(null)
+  useEffect(() => {
+    setDttm(new Date(Date.now()))
+  }, [])
   return (
     <main>
       <Head>
@@ -11,7 +16,7 @@ function IndexPage (props) {
       </Head>
       <h1>Index Page</h1>
       <p>
-        I am a simple static page born in <mark><time dateTime={dttm.toISOString()}>{dttm.toLocaleString()}</time></mark> but it&apos;s <mark><Clock /></mark> now.
+        I am a simple static page born in <mark><time dateTime={dttm?.toISOString()}>{dttm?.toLocaleString()}</time></mark> but it&apos;s <mark><Clock /></mark> now.
       </p>
     </main>
   )
